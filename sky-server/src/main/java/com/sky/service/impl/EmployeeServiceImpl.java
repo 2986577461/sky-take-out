53f639ccd5040976f4b3d3d1df0fddea675de58a
yuchao12333 <14077535+yuchao123333@user.noreply.gitee.com>
2025-04-12 21:38:20 +0800

    增加了添加员工时验证重复的功能

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,21 +1,31 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.Result;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
+import java.time.LocalDateTime;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+   private static BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
 
     @Autowired
     private EmployeeMapper employeeMapper;
@@ -39,7 +49,6 @@ public class EmployeeServiceImpl implements EmployeeService {
             throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
         }
 
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         //密码比对
         if (!encoder.matches(password, employee.getPassword())) {
             //密码错误
@@ -53,6 +62,28 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    @Override
+    public Result addEmployees(EmployeeDTO employeeDTO) {
+
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO,employee);
+
+        employee.setStatus(StatusConstant.ENABLE);
+
+        employee.setPassword(encoder.encode(PasswordConstant.DEFAULT_PASSWORD));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        Long id = BaseContext.getCurrentId();
+        employee.setUpdateUser(id);
+        employee.setCreateUser(id);
+
+        employeeMapper.insert(employee);
+
+        return Result.success();
     }
 
 }
